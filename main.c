d694ed64bf175b3e34679f10f799539a7ccc1cb6
axel <axel@axel-320>
2020-10-27 15:46:23 -0600

    prueba

---
+++ main.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+    int x =45;
+    x=54;
+    
+    printf("Hola mundo %d", x);
+    
+    return (EXIT_SUCCESS);
+}
+
