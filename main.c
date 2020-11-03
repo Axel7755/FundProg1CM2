3922e12d01622614f6ae491173ed8f002d2a48f3
axel <axel@axel-320>
2020-11-03 15:09:36 -0600

    prueba

--- main.c
+++ main.c
@@ -7,8 +7,14 @@ int main(int argc, char** argv) {
     int x =45;
     x=54;
     
-    printf("Hola mundo %d", x);
+    printf("Hola %d \n\n", x);
+    printf("mundo \n");
+    
     
     return (EXIT_SUCCESS);
 }
+//cuando se ejuta una fuicion nos tenemos que apegar a su definicion
+//parametros: elmentos que necesita una funcion para poder trabajar (Argumentos)
 
+
+//bloque definido por parentesis es == a la implementacion de una funcion
