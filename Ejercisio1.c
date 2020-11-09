ee4e7a1b80713b22639b95b93371b03e90fdb290
axel <axel@axel-320>
2020-11-09 10:59:50 -0600

    prueba

---
+++ Ejercisio1.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <math.h>
+
+double x;
+int main(int argc, char** argv) {
+
+    x=(6.18/5)*(pow((14.1-4),2))/2;
+    
+    
+    printf("x sera igua a %f", x);
+    
+    return (0);
+}
+
--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -35,8 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/If_Else.o \
-	${OBJECTDIR}/main.o
+	${OBJECTDIR}/Ejercisio1.o
 
 
 # C Compiler Flags
@@ -63,15 +62,10 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/fundprog1cm2: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/fundprog1cm2 ${OBJECTFILES} ${LDLIBSOPTIONS}
 
-${OBJECTDIR}/If_Else.o: If_Else.c 
+${OBJECTDIR}/Ejercisio1.o: Ejercisio1.c 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
-	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/If_Else.o If_Else.c
-
-${OBJECTDIR}/main.o: main.c 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.c
+	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Ejercisio1.o Ejercisio1.c
 
 # Subprojects
 .build-subprojects:
